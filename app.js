a270d3be5458e2ff94773f5f5b7c5762e72f9519
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-01-31 18:36:06 +0530

    objects and arrays

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-function add(n1, n2, showResult, phrase) {
-    if (showResult) {
-        var result = n1 + n2;
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var num1 = 5;
-var num2 = 1.9;
-var showResult = true;
-var phrase = "Result is ";
-add(num1, num2, showResult, phrase);
+//Here TypeScript automatically infers the type
+// const person = {
+//   name: "Shubh",
+//   age: 19,
+// };
+//Here we are explicitly declaring the type
+var person = {
+    name: "Shubh",
+    age: 19
+};
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log("Done with the basics");
+//Here TypeScript automatically infers the type
+const person = {
+  name: "Shubh",
+  age: 19,
+  hobbies: ["Reading", "Programming"],
+};
+
+//Here we are explicitly declaring the type
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "Shubh",
+//   age: 19,
+// };
+console.log(person.name);
