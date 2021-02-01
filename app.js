3a7023ce2f4227eb9eece8f4ea5c4514c07dac34
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-02-01 12:38:30 +0530

    enum

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+//Using global variables for assigning roles
+// const ADMIN = 0;
+// const AUTHOR = 1;
+//ADMIN receives 0 and AUTHOR 1
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "Authro";
+    Role[Role["AUTHOR"] = void 0] = "AUTHOR";
+})(Role || (Role = {}));
 //Here TypeScript automatically infers the type
-// const person = {
+var person = {
+    name: "Shubh",
+    age: 19,
+    hobbies: ["Reading", "Programming"],
+    role: Role.ADMIN
+};
+//Here we are explicitly declaring the type
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
 //   name: "Shubh",
 //   age: 19,
 // };
-//Here we are explicitly declaring the type
-var person = {
-    name: "Shubh",
-    age: 19
-};
-console.log(person.name);
+console.log(person.name, person.role);
--- app.ts
+++ app.ts
@@ -1,8 +1,19 @@
+//Using global variables for assigning roles
+// const ADMIN = 0;
+// const AUTHOR = 1;
+
+//ADMIN receives 0 and AUTHOR 1
+enum Role {
+  ADMIN,
+  AUTHOR,
+}
+
 //Here TypeScript automatically infers the type
 const person = {
   name: "Shubh",
   age: 19,
   hobbies: ["Reading", "Programming"],
+  role: Role.ADMIN,
 };
 
 //Here we are explicitly declaring the type
@@ -13,4 +24,4 @@ const person = {
 //   name: "Shubh",
 //   age: 19,
 // };
-console.log(person.name);
+console.log(person.name, person.role);
