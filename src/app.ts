c03bb51d06aecd98b43773e1d081ccda7a59c8a2
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-04-29 10:56:50 +0530

    private and public & const shrthand

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,39 @@
 class Department {
-  name: string;
+  //   name: string;
+  private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  //   constructor(n: string) {
+  //     this.name = n;
+  //   }
+
+  //shorthand to creating and init the props using constructor
+  constructor(private id: string, public name: string) {
+    //     this.name = n;
   }
 
   //this can be passed as an argument in function with a type
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department ${this.id}: ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
-console.log(accounting);
+const accounting = new Department("d1", "Accounting");
+accounting.addEmployee("Shubh");
+accounting.addEmployee("Jam");
+
 accounting.describe();
+accounting.printEmployeeInfo();
 
 //This gives an error cause we have passed Department as type in describe function
 // const accountingCopy = { describe: accounting.describe };
 //This will work as there is a name prop inside it which is same as Department
-const accountingCopy = { name: "Shubh", describe: accounting.describe };
-accountingCopy.describe();
+// const accountingCopy = { name: "Shubh", describe: accounting.describe };
+// accountingCopy.describe();
