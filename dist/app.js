ecf58ab11478a543e1b7e1da5d89d434fc954524
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-04-29 10:35:41 +0530

    basic classes

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Learning TypeScript");
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+    //this can be passed as an argument in function with a type
+    describe() {
+        console.log("Department: " + this.name);
+    }
+}
+const accounting = new Department("Accounting");
+console.log(accounting);
+accounting.describe();
+//This gives an error cause we have passed Department as type in describe function
+// const accountingCopy = { describe: accounting.describe };
+//This will work as there is a name prop inside it which is same as Department
+const accountingCopy = { name: "Shubh", describe: accounting.describe };
+accountingCopy.describe();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log("Learning TypeScript");
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  //this can be passed as an argument in function with a type
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department("Accounting");
+console.log(accounting);
+accounting.describe();
+
+//This gives an error cause we have passed Department as type in describe function
+// const accountingCopy = { describe: accounting.describe };
+//This will work as there is a name prop inside it which is same as Department
+const accountingCopy = { name: "Shubh", describe: accounting.describe };
+accountingCopy.describe();
